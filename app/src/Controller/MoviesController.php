dd2e7bca72fd8330cccefb62035e95868b977fa9
mangelperis <mapi_meliana@hotmail.com>
2024-03-12 23:07:24 +0100

    ADD index twig file to show controller query for getALL() landing

--- app/src/Controller/MoviesController.php
+++ app/src/Controller/MoviesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route as RouteAlias;
@@ -10,17 +12,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class MoviesController extends AbstractController
 {
 
-    #[Route('/movies/{name}', name: 'app_movies', defaults: ['name' => null], methods: ['GET', 'HEAD'])]
-    public function index($name): Response
+    private $entityManager;
+
+    function __construct(
+        EntityManagerInterface $entityManager,
+    )
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/movies', name: 'app_movies', methods: ['GET', 'HEAD'])]
+    public function index(): Response
     {
         //Logic
-        //TO DO -> from database select the movies from a table
-
+        //TO DO -> print a list/table with links to each movie
+        $movies = $this->entityManager->getRepository(Movie::class)->findBy(["visible" => 1]);
 
         return $this->render('movies/index.html.twig', [
-            'title' => '',
-            'h1' => $name,
-
+            'title' => 'Movies List',
+            'elements' => \count($movies),
+            'data' => $movies
         ]);
 
     }
